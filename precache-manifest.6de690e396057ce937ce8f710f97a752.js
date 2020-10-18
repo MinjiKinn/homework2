8058883d253c4088aa4a45df31d2d8651b30b951
MinjiKinn <minjikinn819@gmail.com>
2020-10-19 03:54:35 +0900

    Updates

---
+++ precache-manifest.6de690e396057ce937ce8f710f97a752.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ee87e7766af39f01caf2ea910ca4d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55a3720bc37fb660878f",
+    "url": "/static/css/main.638684dc.chunk.css"
+  },
+  {
+    "revision": "ebec896b16b5ac3eed13",
+    "url": "/static/js/2.61c62c72.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.61c62c72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55a3720bc37fb660878f",
+    "url": "/static/js/main.6f9e01ad.chunk.js"
+  },
+  {
+    "revision": "175139c2b43846f1a1fc",
+    "url": "/static/js/runtime-main.79addfa7.js"
+  },
+  {
+    "revision": "211d38148bae18e13cfda6562f07d308",
+    "url": "/static/media/background-pic.211d3814.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
